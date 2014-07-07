c1d5faa44d0d5a60f3bab29a565e0f8129d800df
zfh1005 <zfh1005@126.com>
2014-07-07 18:24:20 +0800

    1.add print Calendar print function in DateFunctionDemo class 2.add GraphicsClassFunctionDemo class 2.add Graphics2DClassFunctionDemo class

--- src/functionDemo/DetaFunctionDemo.java
+++ src/functionDemo/DetaFunctionDemo.java
@@ -7,10 +7,14 @@ package functionDemo;
 
 import java.util.Date;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class DetaFunctionDemo {
 	
 	@SuppressWarnings("deprecation")
+	/*
+	 * print <code>Data</code>
+	 * */
 	public void printDate(){
 		Date start = new Date();
 		//start
@@ -21,6 +25,10 @@ public class DetaFunctionDemo {
 		System.out.println(start.getHours());
 		System.out.println(start.getMinutes());
 	}
+	
+	/*
+	 * print <code>Calendar</code>
+	 * */
 	public void printCalendar(){	
 		Calendar stop = Calendar.getInstance();
 		//end
@@ -31,11 +39,99 @@ public class DetaFunctionDemo {
 		System.out.println(stop.get(Calendar.HOUR));
 		System.out.println(stop.get(Calendar.MINUTE));
 	}
+	
+	/*
+	 * print <code>GregorianCalendar</code>
+	 * */
+	public void printGregarionCalendar(){	
+		GregorianCalendar gregarionCalendar = new GregorianCalendar();
+		//end
+		System.out.println("time is:" + gregarionCalendar.toString());
+		System.out.println(gregarionCalendar.get(Calendar.YEAR));
+		System.out.println(gregarionCalendar.get(Calendar.MONTH) + 1);
+		System.out.println(gregarionCalendar.get(Calendar.DAY_OF_MONTH));
+		System.out.println(gregarionCalendar.get(Calendar.HOUR));
+		System.out.println(gregarionCalendar.get(Calendar.MINUTE));
+	}
+	
+	/*
+	 * <code>Date</code> conversion to <code>Calendar</code> format
+	 * */
+	public Calendar DateConversionToCalendar(Date date){
+		/*
+		 * class <code>GregorianCalendar</code> is also reference <code>Calendar</code>
+		 * */
+		/*
+		GregorianCalendar gregarionCalendar = (GregorianCalendar) GregorianCalendar.getInstance();
+		gregarionCalendar.setTime(date);
+		return gregarionCalendar;
+		**/
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		return calendar;
+	}
+	
+	/*
+	 * <code>Calendar</code> conversion to <code>Date</code> format
+	 * */
+	public Date CalendarConversionToDate(Calendar calendar){
+		Date date = calendar.getTime();
+		return date;
+	}
+	
+	
+	/*
+	 * <code>Calendar</code> demo
+	 * print Calendar
+	 * @param	year	print year
+	 * */
+	public void CalendarDemo(int year){
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.set(Calendar.YEAR, year);
+		System.out.println("#######################");
+		System.out.println("Year:" + year);	
+		System.out.println("#######################");
+		
+		for(int iMonthRty = 0; iMonthRty <= 11; iMonthRty++){
+			gc.set(Calendar.MONTH, iMonthRty);
+			int iMonth = gc.get(Calendar.MONTH);
+			
+			gc.set(Calendar.DAY_OF_MONTH, 1);
+			
+			int iWeekday = gc.get(Calendar.DAY_OF_WEEK);
+			
+			System.out.println("");
+			System.out.println("*******************************");
+			System.out.println("Month:" +(iMonth+1));	
+			System.out.println("**********************");
+			System.out.println("Sun	Mon	Tue	Wed	Thu	Fri	Sta");
+			
+			for(int iWeekRty = Calendar.SUNDAY; iWeekRty < iWeekday; iWeekRty++){
+				System.out.print("	");
+			}
+			
+			do{
+				int Day = gc.get(Calendar.DAY_OF_MONTH);
+				System.out.printf("%3d	", Day);
+				
+				if(iWeekday == Calendar.SATURDAY){
+					System.out.println();
+				}
+				gc.add(Calendar.DAY_OF_MONTH, 1);
+				iWeekday = gc.get(Calendar.DAY_OF_WEEK);
+			}
+			while(gc.get(Calendar.MONTH) == iMonth);
+			System.out.println("");
+			System.out.println("*******************************");
+			gc.add(Calendar.MONTH, iMonthRty);
+		}		
+	}
 
 	public static void main(String[] args) {
 		DetaFunctionDemo dateFD = new DetaFunctionDemo();
-		dateFD.printDate();
-		dateFD.printCalendar();
+		dateFD.CalendarDemo(2013);
+	
 	}
 
 }
