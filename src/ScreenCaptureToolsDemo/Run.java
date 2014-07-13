73466770b56b3bf339c2f4a3c0fb4472841eb2bb
zfh1005 <zfh1005@126.com>
2014-07-13 08:11:18 +0800

    delete Run class TODO list
    delete Run class TODO list

--- src/ScreenCaptureToolsDemo/Run.java
+++ src/ScreenCaptureToolsDemo/Run.java
@@ -15,7 +15,6 @@ public class Run {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		EventQueue.invokeLater(new Runnable() { 
 			@Override
 			public void run() {
