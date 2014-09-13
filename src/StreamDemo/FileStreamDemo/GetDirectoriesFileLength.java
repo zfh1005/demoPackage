22975d8f67214cd320c26b0682747332e3cddae2
zfh1005 <zfh1005@126.com>
2014-09-13 09:49:42 +0800

    add FileStreamDemo package
    1. add GetDirectoriesFileLength.java to FileStreamDemo package. It's
    used for get folder's file length.

---
+++ src/StreamDemo/FileStreamDemo/GetDirectoriesFileLength.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package StreamDemo.FileStreamDemo;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author zfh1005
+ *
+ */
+
+/*
+ * get directories file length
+ * */
+public class GetDirectoriesFileLength {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//if no arguments provided, start at the parent directory
+		if(args.length == 0){
+			args = new String[]{defaultPathString};
+		}
+		
+		try{
+			File pathName = new File(args[0]);
+			String[] fileNamesStrings = pathName.list();
+			
+			//enumerate all files in the directory
+			for(int i = 0; i < fileNamesStrings.length; i++){
+				File file = new File(pathName.getPath(), fileNamesStrings[i]);
+				
+				//if the file is again a directory, call the main recursively
+				if(file.isDirectory()){
+					main(new String[]{file.getPath()});
+				}
+				
+				//if the file is a file, get file full path and space
+				if(file.isFile()){
+					System.out.println(file.getCanonicalPath() + file.separator + file.getName() + "\t\t" + file.length());
+				}
+			}
+		}
+		catch(IOException exception){
+			exception.printStackTrace();
+		}
+	}
+	
+	private static String defaultPathString  = "f:\\code"; 
+
+}
