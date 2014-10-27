5c093e180fd56a041933a9e6eeb9bba21b1ab906
zfh1005 <zfh1005@126.com>
2014-10-27 17:16:19 +0800

    complete package-info class describe
    complete package-info class describe

--- src/XMLDemo/GridBagTest/package-info.java
+++ src/XMLDemo/GridBagTest/package-info.java
@@ -5,4 +5,10 @@
  * @author zfh1005
  *
  */
+/*
+ * this package is shows how to use an XML file to describe a gridbag layout
+ * GridBagTest class is main function interface
+ * FontFrame class is font function
+ * GridBagPane class is pane function
+ * */
 package XMLDemo.GridBagTest;
