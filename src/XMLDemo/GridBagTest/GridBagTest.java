a98f5613aacba921ece7501ee68b25b0526a6a2d
zfh1005 <zfh1005@126.com>
2014-10-27 17:09:26 +0800

    fixed a error
    * if don't change args type to 'final' there is a error
    * "Cannot refer to the non-final local variable args defined in an
    enclosing scope"

--- src/XMLDemo/GridBagTest/GridBagTest.java
+++ src/XMLDemo/GridBagTest/GridBagTest.java
@@ -3,7 +3,12 @@
  */
 package XMLDemo.GridBagTest;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 /**
+ *This program shows how to use an XML file to describe a gridbag layout
  * @author zfh1005
  *
  */
@@ -12,9 +17,19 @@ public class GridBagTest {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(final String[] args) {
+		/*
+		 * if don't change args type to 'final' there is a error
+		 * "Cannot refer to the non-final local variable args defined in an enclosing scope"		 
+		 */
+		EventQueue.invokeLater(new Runnable() {			
+			@Override
+			public void run() {
+				String filenameString = (args.length == 0 ? "fontdialog.xml" : args[0]);
+				JFrame frame = new FontFrame(filenameString);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);				
+			}
+		});
 	}
-
 }
