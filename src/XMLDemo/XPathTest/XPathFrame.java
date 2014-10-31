97664fe5312cf6bec8d1f7a7422cbc4e5e54e1cc
zfh1005 <zfh1005@126.com>
2014-10-31 09:43:08 +0800

    add XPathTest package
    XPathTest package is used demo XPath function

---
+++ src/XMLDemo/XPathTest/XPathFrame.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package XMLDemo.XPathTest;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class XPathFrame extends JFrame{
+
+}
---
+++ src/XMLDemo/XPathTest/XPathTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package XMLDemo.XPathTest;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class XPathTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				JFrame frame = new XPathFrame();
+				
+			}
+		});
+
+	}
+
+}
