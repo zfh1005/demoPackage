828ecf5d97d19adc8de7415d9bd742e9a6e54ecd
zfh1005 <zfh1005@126.com>
2014-11-09 15:45:08 +0800

    add SAXTest package
    This package is used for demo SAX parser used.

---
+++ src/XMLDemo/SAXTest/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author zfh1005
+ *
+ */
+
+/*
+ * This program demonstrates how to use a SAX parser.
+ * This program prints all hyperlinks links of an XHTML web page.
+ */
+
+package XMLDemo.SAXTest;
