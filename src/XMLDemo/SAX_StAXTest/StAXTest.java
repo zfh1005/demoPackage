6fb4088fad3459e421f0f950885c1414543e6f77
zfh1005 <zfh1005@126.com>
2014-11-10 17:12:16 +0800

    complete StAXTest.java
    This class demo used StAX parser XHTML file.

---
+++ src/XMLDemo/SAX_StAXTest/StAXTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package XMLDemo.SAX_StAXTest;
+
+import java.io.InputStream;
+
+import java.net.URL;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class StAXTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception{
+		String urlString ;
+		if(args.length == 0){
+			urlString = "http://www.w3c.org";
+			System.out.println("Using : " + urlString );
+		}
+		else {
+			urlString = args[0];
+		}
+		
+		URL url = new URL(urlString);
+		InputStream inputStream = url.openStream();
+		XMLInputFactory factory = XMLInputFactory.newInstance();
+		XMLStreamReader parserReader = factory.createXMLStreamReader(inputStream);
+		while(parserReader.hasNext()){
+			int event = parserReader.next();
+			if(event == XMLStreamConstants.START_ELEMENT){
+				if(parserReader.getLocalName().equals("a")){
+					String href = parserReader.getAttributeValue(null, "href");
+					if(href != null){
+						System.out.println(href);
+					}
+				}
+			}
+		}
+	}
+}
