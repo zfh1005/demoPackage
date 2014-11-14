daeb0cdbde9c868dc618709e0d99bced418a8036
zfh1005 <zfh1005@126.com>
2014-11-14 08:20:09 +0800

    add XMLDemo.XMLWriteTest package
    This program shows how to write an XML file. It saves a file describing
    a modern drawing in SVG format.

---
+++ src/XMLDemo/XMLWriteTest/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+/*
+ * This program shows how to write an XML file.
+ * It saves a file describing a modern drawing in SVG format.
+ * @version 1.10 2014-11
+ * @author zfh1005
+ * */
+
+package XMLDemo.XMLWriteTest;
