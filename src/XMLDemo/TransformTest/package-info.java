403dda9257eb6b0fb74c6013902f50cbf15efa13
zfh1005 <zfh1005@126.com>
2014-11-24 09:35:07 +0800

    add describe at package-info.java
    add describe at package-info.java

--- src/XMLDemo/TransformTest/package-info.java
+++ src/XMLDemo/TransformTest/package-info.java
@@ -2,7 +2,11 @@
  * 
  */
 /**
+ * This program demonstrates XSL transformations. 
+ * It applies a transformation to a set of employee records.
+ * The records are stored in  the fill employee.dat and turned into XML format.
+ * Specify the style sheet on the command line, e.g. java TransformTest makeprop.xsl.
+ * @version 1.0.0.1 2014-11-24
  * @author zfh1005
- *
  */
 package XMLDemo.TransformTest;
