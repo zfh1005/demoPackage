f4ab077b2f28efd6556bcc06530fb87f1bd57140
zfh1005 <zfh1005@126.com>
2014-11-25 14:38:18 +0800

    Add Network.SocketTest package
    this package is a sample socket demo.

---
+++ src/Network/SocketTest/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * This program makes a socket connection to the atomic clock in Boulder, Colorado, and
+ * prints the time that the server sends.
+ * @version v1.0.0.1
+ * @author zfh1005
+ *
+ */
+package Network.SocketTest;
