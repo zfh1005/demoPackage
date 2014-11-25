75ae5efe93f30992c0b97bb06726ef38aad75ebe
zfh1005 <zfh1005@126.com>
2014-11-25 15:10:00 +0800

    rename SocketTest package
    1. rename SocketTest package to SampleGetTimeSocketTest package.
    2. complete SampleGetTimeSocketTest.java

---
+++ src/Network/SampleGetTimeSocketTest/SampleGetTimeSocketTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package Network.SampleGetTimeSocketTest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * This program makes a socket connection to the atomic clock in Boulder, Colorado, and
+ * prints the time that the server sends.
+ * @author zfh1005
+ *
+ */
+public class SampleGetTimeSocketTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try{
+			Socket socket = new Socket("time-A.timefreq.bldrdoc.gov", 13);
+			try {
+				InputStream inputStream = socket.getInputStream();
+				Scanner in = new Scanner(inputStream);
+				
+				while(in.hasNextLine()){
+					String line = in.nextLine();
+					System.out.print(line);
+				}
+				in.close();
+			} 
+			finally{
+				socket.close();
+			}
+		}
+		catch(IOException exception){
+			exception.printStackTrace();
+		}
+	}
+
+}
---
+++ src/Network/SampleGetTimeSocketTest/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @version v1.0.0.1
+ * @author zfh1005
+ *
+ */
+package Network.SampleGetTimeSocketTest;
