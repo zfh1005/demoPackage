a683b6ee1171daa6c7ba0970b800c73314bf627d
zfh1005 <zfh1005@126.com>
2014-11-27 14:32:41 +0800

    add InetAddressTest class
    this class is used for demo InetAddress class.

---
+++ src/Network/SocketTest/InetAddressTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Network.SocketTest;
+
+
+/**
+ * This program demonstrates the InterAddress class.
+ * Supply a host name as command line argument, or run
+ * without command line argument to see the address of the local host.
+ * @version 1.0.0.1
+ * @author zfh1005
+ *
+ */
+public class InetAddressTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+	}
+
+}
