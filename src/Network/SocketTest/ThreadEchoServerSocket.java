e57fef6acb94fe3c180cfdbf7051a1c29d692d68
zfh1005 <zfh1005@126.com>
2014-11-28 16:14:06 +0800

    add ThreadEchoServerSocket class
    ThreadEchoServerSocket class is used for listens to port 8189 and echo
    back all client input.

---
+++ src/Network/SocketTest/ThreadEchoServerSocket.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Network.SocketTest;
+
+/**
+ * This program implements a multithread server that
+ * listens to port 8189 and echo back all client input.
+ * @version 1.0.0.1
+ * @author zfh1005
+ *
+ */
+public class ThreadEchoServerSocket {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
