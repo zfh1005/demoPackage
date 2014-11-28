c1ab2ee648d8a2282158bbca228d631094611362
zfh1005 <zfh1005@126.com>
2014-11-28 15:19:34 +0800

    add EchoServerSocket class
    EchoServerSocket class is used forlistens to port 8189 and echoes back
    all client input.

---
+++ src/Network/SocketTest/EchoServerSocket.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Network.SocketTest;
+
+/**
+ * This program implements a simple server socket that
+ * listens to port 8189 and echoes back all client input.
+ * @version 1.0.0.1 
+ * @author zfh1005
+ *
+ */
+public class EchoServerSocket {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
