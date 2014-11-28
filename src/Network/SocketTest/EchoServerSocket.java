133699acc9be30ad2cf0e5948df454696eb220d4
zfh1005 <zfh1005@126.com>
2014-11-28 15:53:37 +0800

    Complete EchoServerSocket class
    Complete EchoServerSocket class.

--- src/Network/SocketTest/EchoServerSocket.java
+++ src/Network/SocketTest/EchoServerSocket.java
@@ -3,6 +3,14 @@
  */
 package Network.SocketTest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * This program implements a simple server socket that
  * listens to port 8189 and echoes back all client input.
@@ -16,7 +24,39 @@ public class EchoServerSocket {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try{
+			//establish server socket
+			ServerSocket serverSocket = new ServerSocket(8189);
+			
+			//wait for client connection
+			Socket incomingSocket = serverSocket.accept();
+			
+			try {
+				InputStream inputStream = incomingSocket.getInputStream();
+				OutputStream outputStream = incomingSocket.getOutputStream();
+				
+				Scanner inScanner = new Scanner(inputStream);
+				PrintWriter outPrintWriter = new PrintWriter(outputStream, true);
+				
+				outPrintWriter.println("Hello! Entry BYE to exit.");
+				
+				//echo client input
+				boolean done = false;
+				while(!done && inScanner.hasNextLine()){
+					String line = inScanner.nextLine();
+					outPrintWriter.println("Echo: " + line);
+					if(line.trim().equals("BYE")){
+						done = true;
+					}
+				}
+			}
+			finally{
+				incomingSocket.close();
+			}
+		}
+		catch(IOException exception){
+			exception.printStackTrace();
+		}
 
 	}
 
