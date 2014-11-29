462f2232364cb9d6f15bb0049b0d8fe751f69932
zfh1005 <zfh1005@126.com>
2014-11-29 10:36:20 +0800

    Add HalfCloseSocket class
    HalfCloseSocket class is used for show half-close function works.

---
+++ src/Network/SocketTest/HalfCloseSocket.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Network.SocketTest;
+
+/**
+ * This class is demo show half-close function.
+ * @author zfh1005
+ *
+ */
+public class HalfCloseSocket {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
