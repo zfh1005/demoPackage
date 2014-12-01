37d09a9cc6c8fef1e0a720f8d416d9f3e4c701e3
zfh1005 <zfh1005@126.com>
2014-12-01 09:49:02 +0800

    Complete class InterruptibleSocketTest
    class InterruptibleSocketTest is main function for this program.

--- src/Network/InterruptibleSocketTest/InterruptibleSocketTest.java
+++ src/Network/InterruptibleSocketTest/InterruptibleSocketTest.java
@@ -3,6 +3,10 @@
  */
 package Network.InterruptibleSocketTest;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 /**
  * @author zfh1005
  *
@@ -13,8 +17,16 @@ public class InterruptibleSocketTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				JFrame frame = new InterruptibleSocketFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+				
+			}
+		});
 	}
 
 }
