bde341c68640517156db5aed137f8c5f745ecd5e
zfh1005 <zfh1005@126.com>
2014-12-03 11:09:14 +0800

    Complete TestServerHandler class
    This class handles the client input for one server socket connection.

--- src/Network/InterruptibleSocketTest/TestServerHandler.java
+++ src/Network/InterruptibleSocketTest/TestServerHandler.java
@@ -3,19 +3,54 @@
  */
 package Network.InterruptibleSocketTest;
 
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JTextArea;
+
 /**
+ * This class handles the client input for one server socket connection.
  * @author zfh1005
  *
  */
 public class TestServerHandler implements Runnable{
 
-	/* (non-Javadoc)
+	/*
+	 * Constructs a handler.
+	 * */
+	public TestServerHandler(Socket iSocket, JTextArea message){
+		incomingSocket = iSocket;
+		messageArea = message;
+	}
+	
+	/* 
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		try{
+			OutputStream outputStream = incomingSocket.getOutputStream();
+			PrintWriter outPrintWriter = new PrintWriter(outputStream, true);
+			while(counter < 100){
+				counter ++;
+				if(counter <= 10){
+					outPrintWriter.println(counter);
+				}
+				Thread.sleep(100);
+			}
+			
+			incomingSocket.close();
+			messageArea.append("Closing server\n.");
+		}
+		catch(Exception exception){
+			messageArea.append("\nTestServerHandler.run: " + exception);
+		}		
 	}
+	
+	
+	private Socket incomingSocket;
+	private int counter;
+	private JTextArea messageArea;
 
 }
