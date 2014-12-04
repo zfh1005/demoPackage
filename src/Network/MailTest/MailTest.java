7ac3621dc0ddb5194b3543fa721050c13203b03c
zfh1005 <zfh1005@126.com>
2014-12-04 15:30:35 +0800

    Complete MailTest class.
    MailTest class is main function for this package.

--- src/Network/MailTest/MailTest.java
+++ src/Network/MailTest/MailTest.java
@@ -3,6 +3,10 @@
  */
 package Network.MailTest;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 /**
  * @author zfh1005
  *
@@ -13,7 +17,15 @@ public class MailTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				JFrame frame = new MailTestFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
+				frame.setVisible(true);
+			}
+		});
 
 	}
 
