d406fd3dacff8d6d89ef587d3ae603c6f93c94b3
zfh1005 <zfh1005@126.com>
2014-12-04 15:05:11 +0800

    Complete MailTest package constructs.
    1. Add MailTest class.
    2. Add MailTestFrame class.

---
+++ src/Network/MailTest/MailTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Network.MailTest;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class MailTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Network/MailTest/MailTestFrame.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Network.MailTest;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class MailTestFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4579387454147561471L;
+
+}
