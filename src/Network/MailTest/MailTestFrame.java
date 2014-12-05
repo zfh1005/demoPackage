ea3378efc433582f2ca502e1353bd9a70a97def3
zfh1005 <zfh1005@126.com>
2014-12-05 08:39:29 +0800

    add some code to MailTestFrame class
    add some code to MailTestFrame class.

--- src/Network/MailTest/MailTestFrame.java
+++ src/Network/MailTest/MailTestFrame.java
@@ -3,9 +3,28 @@
  */
 package Network.MailTest;
 
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingWorker;
+
+import org.omg.CORBA.COMM_FAILURE;
+
+import SwingDemo.GBC;
 
 /**
+ * The frame for the mail GUI
  * @author zfh1005
  *
  */
@@ -15,5 +34,87 @@ public class MailTestFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = -4579387454147561471L;
-
+	public MailTestFrame(){
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		setTitle("MailTest");
+		
+		setLayout(new GridBagLayout());
+		
+		add(new JLabel("From:"), new GBC(0, 0).setFill(GBC.HORIZONTAL));
+		frommField = new JTextField(20);
+		add(frommField, new GBC(1, 0).setFill(GBC.HORIZONTAL).setWeight(100, 0));
+	
+		add(new JLabel("To:"), new GBC(0, 1).setFill(GBC.HORIZONTAL));
+		toField = new JTextField(20);
+		add(toField, new GBC(1, 1).setFill(GBC.HORIZONTAL).setWeight(100, 0));
+		
+		add(new JLabel("SMTP Server:"), new GBC(0, 2).setFill(GBC.HORIZONTAL));
+		smtpServer = new JTextField(20);
+		add(smtpServer, new GBC(1, 2).setFill(GBC.HORIZONTAL).setWeight(100, 0));
+		
+		messageArea = new JTextArea();
+		add(new JScrollPane(), new GBC(0, 3, 2, 1).setFill(GBC.BOTH).setWeight(100, 100));
+		
+		comnArea = new JTextArea();
+		add(new JScrollPane(), new GBC(0, 4, 2, 1).setFill(GBC.BOTH).setWeight(100, 100));
+	
+		JPanel buttonpPanel = new JPanel();
+		add(buttonpPanel, new GBC(0, 5, 2, 1));
+		
+		JButton sendbButton = new JButton("Send");
+		buttonpPanel.add(sendbButton);
+		sendbButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new SwingWorker<Void, Void>() {
+					@Override
+					protected Void doInBackground() throws Exception {
+						comnArea.setText("");
+						sendMail();
+						return null;
+					}
+				}.execute();
+			}
+		});
+	}
+	
+	/*
+	 * Sends the mail message that has been authored in the GUI.
+	 * */
+	public void sendMail() {
+		try {
+			
+			
+		} catch (Exception e) {
+			comnArea.append("Error:" + e);
+		}
+	}
+	
+	public void send(String string) throws IOException {
+		comnArea.append(string);
+		comnArea.append("\n");
+		outPrintWriter.print(string.replace("\n", "\r\n"));
+		outPrintWriter.print("\r\n");
+		outPrintWriter.flush();
+	}
+	
+	public void receive() throws IOException{
+		String line = inScanner.nextLine();
+		comnArea.append(line);
+		comnArea.append("\n");
+	}
+	
+	private Scanner inScanner;
+	private PrintWriter outPrintWriter;
+	private JTextField frommField;
+	private JTextField toField;
+	private JTextField smtpServer;
+	private JTextArea messageArea;
+	private JTextArea comnArea;
+	
+	private static final int DEFAULT_WIDTH = 300;
+	private static final int DEFAULT_HEIGHT = 300;
+	
+	
 }
