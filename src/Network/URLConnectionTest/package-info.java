a257705b4ef6f67e4d3d4bd59065f35c39c1965c
zfh1005 <zfh1005@126.com>
2014-12-07 16:11:42 +0800

    add Network.URLConnectionTest package
    add Network.URLConnectionTest package for demo URL connection.

---
+++ src/Network/URLConnectionTest/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * This program connects to a URL and displays the response header data
+ * and the first 10 lines of the requested data.
+ * 
+ * Supply the URL and an optional user name and password(for HTTP basic authentication)
+ * on the command line 
+ * @author zfh1005
+ *
+ */
+package Network.URLConnectionTest;
