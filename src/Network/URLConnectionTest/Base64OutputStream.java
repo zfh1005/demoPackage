d1d20d0065b0f51afe7652e7a8d319e503898376
zfh1005 <zfh1005@126.com>
2014-12-09 09:34:14 +0800

    add some code to Base64OutputStream class
    add some code to Base64OutputStream class

--- src/Network/URLConnectionTest/Base64OutputStream.java
+++ src/Network/URLConnectionTest/Base64OutputStream.java
@@ -24,5 +24,17 @@ public class Base64OutputStream extends FilterOutputStream{
 		super(out);
 		// TODO Auto-generated constructor stub
 	}
+	
+	private static char[] toBase64 = {
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+	};
+	
+	private int col = 0;
+	private int i = 0;
+	private int[] inBuf = new int[3];
 
 }
