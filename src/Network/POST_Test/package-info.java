6c8ed47014ce849d156d06833b9a3938affd9b7e
zfh1005 <zfh1005@126.com>
2014-12-10 17:06:18 +0800

    add Network.POST_Test package
    This program demonstrates how to use the URLConnection class for a POST
    request.

---
+++ src/Network/POST_Test/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * This program demonstrates how to use the URLConnection class for a POST request.
+ * @version 1.0.0.1 2014/12/10 
+ * 
+ * @author zfh1005
+ *
+ */
+package Network.POST_Test;
