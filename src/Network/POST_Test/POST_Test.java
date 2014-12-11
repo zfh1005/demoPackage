fa24c8ec96a363bcfd042e4ec943c51da744a62c
zfh1005 <zfh1005@126.com>
2014-12-11 08:16:16 +0800

    complete program structs
    complete program structs

---
+++ src/Network/POST_Test/POST_Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Network.POST_Test;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class POST_Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Network/POST_Test/PostTestFrame.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package Network.POST_Test;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class PostTestFrame extends JFrame {
+
+}
