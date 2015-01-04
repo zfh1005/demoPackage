81720e75727b7b3e719961ba12c2fafe8f1b2e8f
zfh1005 <zfh1005@126.com>
2015-01-04 11:29:32 +0800

    add SQL.ExecSQL package
    This package is used for Executes all SQL statement in a file.

---
+++ src/SQL/ExecSQL/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * Executes all SQL statement in a file.
+ * Call this program as <br>
+ * java -classpath driverPath:.ExecSQL commandFile
+ * @version v1.0 2015-01-04
+ * @author zfh1005
+ *
+ */
+package SQL.ExecSQL;
