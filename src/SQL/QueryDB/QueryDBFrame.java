1d75339a83181b8e1dfe753ed440e72aebc53654
zfh1005 <zfh1005@126.com>
2015-03-31 09:45:21 +0800

    add QueryDBFrame.java into package
    1. the class is a empty class, need fill the code into it.
    2. the package full path name is : demoPackage\src\SQL\QueryDB

---
+++ src/SQL/QueryDB/QueryDBFrame.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package SQL.QueryDB;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zfh1005
+ * @date 2015/03/31
+ * @version v0.0.1
+ *
+ */
+public class QueryDBFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7996420834616566497L;
+
+}
