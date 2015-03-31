536c0eb10999d43e86d23097c5aa08a581da8e27
zfh1005 <zfh1005@126.com>
2015-03-31 15:50:37 +0800

    add date define into QueryDBFrame
    1. add date define into file

--- src/SQL/QueryDB/QueryDBFrame.java
+++ src/SQL/QueryDB/QueryDBFrame.java
@@ -3,7 +3,13 @@
  */
 package SQL.QueryDB;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * @author zfh1005
@@ -18,4 +24,53 @@ public class QueryDBFrame extends JFrame {
 	 */
 	private static final long serialVersionUID = -7996420834616566497L;
 
+	
+	//date define
+	
+	public static final int DEFAULT_WIDTH = 400;
+	public static final int DEFAULT_HEIGHT = 400;
+	
+	private JComboBox authorsJComboBox;
+	private JComboBox publishersJComboBox;
+	private JTextField priceChangeJTextField;
+	private JTextArea resulJTextArea;
+	private Connection connection;
+	private PreparedStatement authorQueryStatement;
+	private PreparedStatement authorPublisherQueryStatement;
+	private PreparedStatement publisherQueryStatement;
+	private PreparedStatement allQueryStatement;
+	private PreparedStatement priceUpdateStatement;
+	
+	private static final String authorPublisherQuery = 
+			"SELECT Books.Price, Books.Title FROM Books, BooksAuthors, Authors, Publishers"
+			+ "WHERE Authors.Author_Id = BooksAuthors.Author_Id"
+			+ "AND BooksAuthors.ISBN = Books.ISBN"
+			+ "AND Books.Publisher_Id = Publishers.Publisher_Id"
+			+ "AND Authors.Name = ?"
+			+ "AND Publishers.Name = ?";
+			
+	private static final String authorQuery = 
+			"SELECT Books.Price, Books.Title FROM Books, BooksAuthors, Authors"
+			+ "WHERE Author.Author_Id = BooksAuthors.Author_Id"
+			+ "AND Books BooksAuthors.ISBN = Books.ISBN"
+			+ "Authors.Name = ?";
+	
+	private static final String publisherQuery = 
+			"SELECT Books.Price, Books.Title FROM Books, Publishers"
+			+ "WHERE Author.Author_Id = BooksAuthors.Author_Id"
+			+ "Authors.Name = ?";
+	
+	private static final String allQuery = 
+			"SELECT Books.Price, Books.Title FROM Books";
+	
+	private static final String priceUpdate = 
+			"UPDATE Books "
+			+ "SET Price = Price + ?"
+			+ "WHERE Books.Publisher_Id = (SELECT Publisher_Id FROME Publishers WHERE Name = ? )";
+	
+	
+	
+	
+	
+	
 }
