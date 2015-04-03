f9db734d3262b1eb5e29958f31d17f8d46e86d27
zfh1005 <zfh1005@126.com>
2015-04-03 08:50:16 +0800

    add mian function into project
    add mian function into project

---
+++ src/SQL/QueryDB/ViewDB.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package SQL.QueryDB;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class ViewDB {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				JFrame frame = new ViewDBFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);				
+			}
+		});
+
+	}
+
+}
---
+++ src/SQL/QueryDB/ViewDBFrame.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package SQL.QueryDB;
+
+import javax.swing.JFrame;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class ViewDBFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3077875055654780889L;
+
+}
