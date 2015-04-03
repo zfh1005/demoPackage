e6cb0f4b2777370e8f06aad6b3cfdf77a618c9a7
zfh1005 <zfh1005@126.com>
2015-04-03 14:36:07 +0800

    add 'DataPanel.java' into package
    1. add 'DataPanel.java' into package
    2. the package full path name is : demoPackage\src\SQL\ViewDB

---
+++ src/SQL/ViewDB/DataPanel.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package SQL.ViewDB;
+
+import javax.swing.JPanel;
+
+/**
+ * @author zfh1005
+ *
+ */
+public class DataPanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3706776035748986715L;
+
+}
